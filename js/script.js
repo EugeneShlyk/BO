866e322741e8438f570b990aa4cf5ee57ab27e72
Evgene Shlyk <serpupstars@gmail.com>
2023-06-03 22:48:43 +1000

    начал наконец кодить сайт

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const navigation = document.querySelector('.header__navigation');
+const navToggle = document.querySelector('.header__navigation-toggle');
+
+navigation.classList.remove('.header__navigation--nojs');
+
+navToggle.addEventListener('click', = () => {
+    if ()
+})
