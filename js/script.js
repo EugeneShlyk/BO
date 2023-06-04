734a64059d3319a5144a1a28f7ba9cfb246b67b1
Evgene Shlyk <serpupstars@gmail.com>
2023-06-05 07:27:18 +1000

    утро понедельника 2

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const navigation = document.querySelector('.header__navigation');
-const navToggle = document.querySelector('.header__navigation-toggle');
-
-navigation.classList.remove('.header__navigation--nojs');
-
-navToggle.addEventListener('click', = () => {
-    if ()
-})
