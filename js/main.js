5101412a3d702d48059e45ab1ec0e43ecf947cc8
Evgene Shlyk <serpupstars@gmail.com>
2023-06-06 07:27:02 +1000

    утро вторника дом

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { onShowNavigation } from "./navigation.js";
 
-const navToggle = document.querySelector(".main-nav-toggle");
+const navToggle = document.querySelector(".main-nav__toggle");
 
 navToggle.addEventListener("click", onShowNavigation);
--- js/navigation.js
+++ js/navigation.js
@@ -2,15 +2,19 @@ const navigation = document.querySelector(".header__main-nav");
 const navToggle = document.querySelector(".main-nav-toggle");
 
 navigation.classList.remove("main-nav--nojs");
+navigation.classList.add("main-nav--closed");
 
 const onShowNavigation = () => {
-  if (navigation.classList.contains("navigation__opened")) {
-    navigation.classList.remove("navigation__opened");
-    navigation.classList.add("navigation--closed");
+  if (navigation.classList.contains("main-nav--closed")) {
+    navigation.classList.remove("main-nav--closed");
+    navigation.classList.add("main-nav--opened");
   } else {
-    navigation.classList.remove("navigation--closed");
-    navigation.classList.add("navigation__opened");
+    navigation.classList.remove("main-nav--opened");
+    navigation.classList.add("main-nav--closed");
   }
 };
 
 export { onShowNavigation };
+
+// main-nav--closed
+// main-nav--opened
